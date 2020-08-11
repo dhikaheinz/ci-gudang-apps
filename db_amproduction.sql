106beeeeddeedf36324b8738f3bcd89a28ea53a1
dhikaheinz <adhika.bhisana@gmail.com>
2020-08-11 21:14:05 +0700

    New Database

--- db_amproduction.sql
+++ db_amproduction.sql
@@ -18,36 +18,63 @@ CREATE TABLE IF NOT EXISTS `p_category` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id_category`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table db_amproduction.p_category: ~2 rows (approximately)
 /*!40000 ALTER TABLE `p_category` DISABLE KEYS */;
 INSERT INTO `p_category` (`id_category`, `name`, `created`, `updated`) VALUES
-	(1, 'kaos kerah', '2020-07-30 01:23:04', '2020-07-31 01:23:04'),
-	(4, 'kaos oblong', '2020-07-31 20:03:54', '2020-08-02 09:55:40');
+	(5, 'Polo', '2020-08-11 20:49:28', '2020-08-11 13:56:12'),
+	(6, 'APD', '2020-08-11 20:49:39', '2020-08-11 13:56:17'),
+	(7, 'Oblong', '2020-08-11 20:49:54', '2020-08-11 13:56:23'),
+	(8, 'Lengan Panjang', '2020-08-11 20:50:09', '2020-08-11 13:56:30'),
+	(9, 'Partai', '2020-08-11 20:50:20', '2020-08-11 13:56:45');
 /*!40000 ALTER TABLE `p_category` ENABLE KEYS */;
+
+-- Dumping structure for table db_amproduction.p_color
+CREATE TABLE IF NOT EXISTS `p_color` (
+  `id_color` int(11) NOT NULL AUTO_INCREMENT,
+  `color` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_color`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_amproduction.p_color: ~0 rows (approximately)
+/*!40000 ALTER TABLE `p_color` DISABLE KEYS */;
+INSERT INTO `p_color` (`id_color`, `color`) VALUES
+	(1, 'Merah'),
+	(4, 'Kuning'),
+	(5, 'Hijau'),
+	(6, 'Biru'),
+	(7, 'Hitam'),
+	(8, 'Putih');
+/*!40000 ALTER TABLE `p_color` ENABLE KEYS */;
 
 -- Dumping structure for table db_amproduction.p_item
 CREATE TABLE IF NOT EXISTS `p_item` (
   `id_item` int(11) NOT NULL AUTO_INCREMENT,
   `barcode` varchar(128) DEFAULT NULL,
   `name` varchar(128) DEFAULT NULL,
-  `category` int(11) NOT NULL,
-  `price` int(11) DEFAULT NULL,
-  `stock` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `category` int(11) DEFAULT NULL,
+  `color` int(11) DEFAULT NULL,
+  `size` varchar(10),
+  `price` int(11),
+  `stock` int(11) DEFAULT '0',
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id_item`),
   UNIQUE KEY `barcode` (`barcode`),
   KEY `category` (`category`),
+  KEY `color` (`color`),
+  CONSTRAINT `FK_p_item_p_color` FOREIGN KEY (`color`) REFERENCES `p_color` (`id_color`),
   CONSTRAINT `p_item_ibfk_1` FOREIGN KEY (`category`) REFERENCES `p_category` (`id_category`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table db_amproduction.p_item: ~2 rows (approximately)
+-- Dumping data for table db_amproduction.p_item: ~0 rows (approximately)
 /*!40000 ALTER TABLE `p_item` DISABLE KEYS */;
-INSERT INTO `p_item` (`id_item`, `barcode`, `name`, `category`, `price`, `stock`, `created`, `updated`) VALUES
-	(1, 'A1', 'kaos oblong', 4, 120000, 40, '2020-08-06 10:10:08', NULL),
-	(2, 'A2', 'kaos gandul', 4, 20000, 5, '2020-08-06 10:58:03', NULL);
+INSERT INTO `p_item` (`id_item`, `barcode`, `name`, `category`, `color`, `size`, `price`, `stock`, `created`, `updated`) VALUES
+	(8, 'A1', 'Baju Partai', 9, 1, 'XS', 80000, 100, '2020-08-11 20:57:12', NULL),
+	(9, 'A2', 'Baju APD', 6, 8, 'M', 100000, 100, '2020-08-11 21:00:55', NULL),
+	(10, 'A3', 'Baju Muslim', 8, 8, '', 130000, 790, '2020-08-11 21:02:12', NULL),
+	(11, 'A4', 'Baju E-Sport', 8, 1, 'S', 150000, 0, '2020-08-11 21:03:18', NULL);
 /*!40000 ALTER TABLE `p_item` ENABLE KEYS */;
 
 -- Dumping structure for table db_amproduction.supplier
@@ -65,8 +92,7 @@ CREATE TABLE IF NOT EXISTS `supplier` (
 -- Dumping data for table db_amproduction.supplier: ~2 rows (approximately)
 /*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
 INSERT INTO `supplier` (`id_supplier`, `name`, `phone`, `address`, `description`, `created`, `updated`) VALUES
-	(5, 'Toko A', '09809080989', 'AMPEL', 'PEMASOK KAOS OBLONG', '2020-08-06 09:55:40', '2020-08-06 14:07:08'),
-	(6, 'Toko B', '09809080989', 'Gumpang', 'PEMASOK KAOS KERAH', '2020-08-06 09:56:03', '2020-08-06 14:07:25');
+	(7, 'CV AMProduction', '081236715261', 'Lawean', 'Pemasok Baju', '2020-08-11 20:48:49', '2020-08-11 13:49:09');
 /*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
 
 -- Dumping structure for table db_amproduction.t_stock
@@ -84,17 +110,17 @@ CREATE TABLE IF NOT EXISTS `t_stock` (
   KEY `item` (`item`),
   CONSTRAINT `t_stock_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`id_supplier`),
   CONSTRAINT `t_stock_ibfk_2` FOREIGN KEY (`item`) REFERENCES `p_item` (`id_item`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table db_amproduction.t_stock: ~7 rows (approximately)
+-- Dumping data for table db_amproduction.t_stock: ~0 rows (approximately)
 /*!40000 ALTER TABLE `t_stock` DISABLE KEYS */;
 INSERT INTO `t_stock` (`id_stock`, `item`, `type`, `detail`, `supplier`, `qty`, `date`, `created`) VALUES
-	(9, 1, 'in', 'kulakan', NULL, 36, '2020-08-06', '2020-08-06 12:02:40'),
-	(13, 1, 'out', 'jk', NULL, 8, '2020-08-06', '2020-08-06 12:52:41'),
-	(15, 2, 'in', 'pemasok', 5, 5, '2020-08-06', '2020-08-06 13:01:03'),
-	(16, 1, 'in', 'baju masuk', 5, 10, '2020-08-06', '2020-08-06 20:31:41'),
-	(17, 1, 'in', 'baju masuk', 5, 10, '2020-08-06', '2020-08-06 20:32:45'),
-	(18, 1, 'out', 'baju keluar', NULL, 8, '2020-08-06', '2020-08-06 20:33:39');
+	(27, 8, 'in', 'Barang Masuk ', 7, 100, '2020-08-11', '2020-08-11 21:04:39'),
+	(28, 9, 'in', 'Barang Masuk ', 7, 150, '2020-08-11', '2020-08-11 21:07:04'),
+	(29, 10, 'in', 'Barang Masuk ', 7, 2000, '2020-08-11', '2020-08-11 21:07:35'),
+	(30, 10, 'out', 'Baju Keluar', NULL, 1200, '2020-08-11', '2020-08-11 21:08:42'),
+	(31, 9, 'out', 'Baju Keluar', NULL, 50, '2020-08-11', '2020-08-11 21:09:07'),
+	(32, 10, 'out', 'Baju Keluar', NULL, 10, '2020-08-11', '2020-08-11 21:09:33');
 /*!40000 ALTER TABLE `t_stock` ENABLE KEYS */;
 
 -- Dumping structure for table db_amproduction.user
@@ -111,8 +137,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table db_amproduction.user: ~2 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `username`, `password`, `nama`, `address`, `level`) VALUES
-	(1, 'admin', 'admin', 'han', 'ampel', 1),
-	(2, 'kasir', 'kasir', 'kasir', 'ampel', 2);
+	(1, 'admin', 'admin', 'Admin', 'Lawean', 1),
+	(3, 'kasir', 'kasir', 'Kasir', 'Lawean', 2);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
